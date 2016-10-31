9b0e9bf3310fb7a2c881f52ce54a246ac8d8b75d
jecourtney <JCourtney@unitedmcgill.com>
2016-10-31 15:34:20 -0400

    Initial commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,45 @@
+import { NgModule }                     from '@angular/core';
+import { BrowserModule }                from '@angular/platform-browser';
+import { LocationStrategy,
+         PathLocationStrategy }         from '@angular/common';
+
+import { AppComponent }                 from './app.component';
+import { Ng2BootstrapModule }           from 'ng2-bootstrap/ng2-bootstrap';
+import { NAV_DROPDOWN_DIRECTIVES }      from './shared/nav-dropdown.directive';
+
+import { ChartsModule }                 from 'ng2-charts/ng2-charts';
+import { SIDEBAR_TOGGLE_DIRECTIVES }    from './shared/sidebar.directive';
+import { AsideToggleDirective }         from './shared/aside.directive';
+import { BreadcrumbsComponent }         from './shared/breadcrumb.component';
+
+// Routing Module
+import { AppRoutingModule }             from './app.routing';
+
+//Layouts
+import { FullLayoutComponent }          from './layouts/full-layout.component';
+
+// My Components (Move to modules that need them...)
+
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        AppRoutingModule,
+        Ng2BootstrapModule,
+        ChartsModule
+    ],
+    declarations: [
+        AppComponent,
+        FullLayoutComponent,
+        NAV_DROPDOWN_DIRECTIVES,
+        BreadcrumbsComponent,
+        SIDEBAR_TOGGLE_DIRECTIVES,
+        AsideToggleDirective
+    ],
+    providers: [{
+        provide: LocationStrategy,
+        useClass: PathLocationStrategy
+    }],
+    bootstrap: [ AppComponent ]
+})
+export class AppModule { }
