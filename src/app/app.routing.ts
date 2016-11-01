159f139538651bd8993c31ab28079d35c2e526cb
jecourtney <JCourtney@unitedmcgill.com>
2016-11-01 12:00:23 -0400

    Locations

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -22,6 +22,10 @@ export const routes: Routes = [
                 path: 'dashboard',
                 loadChildren: 'app/dashboard/dashboard.module#DashboardModule'
             },
+            {
+                path: 'location',
+                loadChildren: 'app/location/location.module#LocationModule'
+            }
         ]
     }
 ];
