1582052ba61d6c9baf9e8e9d0df54dc63d45d262
jecourtney <JCourtney@unitedmcgill.com>
2016-11-07 16:11:58 -0500

    get and post working with cors on server

--- src/app/contactus/contactus.service.ts
+++ src/app/contactus/contactus.service.ts
@@ -3,6 +3,7 @@ import  { Http, Response, Headers, RequestOptions } from '@angular/http';
 import { ConfigService } from "../services/config.service";
 import { ContactUs } from '../models/contact-us'
 import {Observable} from 'rxjs/Rx';
+import { Values } from '../models/values';
 
 @Injectable()
 export class ContactUsService{
@@ -13,20 +14,60 @@ export class ContactUsService{
         this.config = configService.config;
     }
 
+    public getValues() : Observable<Values[]>{
+        var userData : any;
+
+        // return this.http.get("http://cs1213:5001/api/values")
+        // .map(res=>{
+        //     if (res.status != 200){
+        //         throw new Error('Error' + res.status);
+        //     }
+        //     else
+        //     {
+        //         return res.json();
+        //     }
+        // })
+
+        return this.http.get("http://localhost:5001/api/values")
+                   .map((res:Response)=>res.json())
+                   .catch((error:any)=>Observable.throw(error.json().error||'Server error'));
+
+                //    .map(data => {
+                //      userData = data;
+                //      console.log(userData);
+                //     })
+    }
+
     public sendMessage(contact : ContactUs){
+       
         let bodyString = JSON.stringify(contact); // Stringify payload
         let headers = new Headers({ 'Content-Type': 'application/json' }); // ... Set content type to JSON
-        let options = new RequestOptions({ headers: headers }); // Create a request option
-        let url = this.config.apiUrl+"/contactus"
-        this.http.post(url, bodyString, options)
-        .map((response:Response) => response.json())
-        .catch(this._handleError);
+        let url = this.config.apiUrl+"/contactus";
+        //let options = new RequestOptions({ headers: headers, method: "post" }); // Create a request option
+        //.map((response:Response) => response.json())
 
-        alert(url + ":" + bodyString);
+        this.http.post(url, bodyString, {headers:headers})      
+        .map((res:Response) => {
+            console.log(res.json());
+            return res.json();})
+        .catch(this._handleError)
+        .subscribe(
+                data => console.log(data),
+                err => console.log(err)
+            );
+        
+
+        //alert(url + ":" + bodyString);
     }
 
-    private _handleError(err:any){
-        console.log(err);
-        return Observable.throw(err);
+    private _handleError(error:any){
+        console.error(error);
+        return Observable.throw(error.json().error || ' error');
     }
+
+    private extractData(res: Response) {
+        let body = res.json();
+        return body.data || { };
+    } 
+
 }
