51b05cd1848b080d0efa5da40762c090aa40eea5
jecourtney <JCourtney@unitedmcgill.com>
2017-03-23 10:04:44 -0400

    fixed files

--- src/app/files/files.component.ts
+++ src/app/files/files.component.ts
@@ -11,47 +11,8 @@ import { ConfigService } from "../services/config.service";
 })
 export class FilesComponent implements OnInit {
 
-  public locationCode : string;
-  private url : string;
-  //public uploader : FileUploader;
-  //public hasAnotherDropZoneOver:boolean = false;
-  private config : any;
-  
-  private zone: NgZone;
-  private basicOptions: Object;
-  public progress: number = 0;
-  public response: any = {};
-  public speed: string;
-
-  constructor(private configService: ConfigService){
-        this.config = configService.config;
-  }
-
-  public handleUpload(data: any): void {
-    this.zone.run(() => {
-      this.response = data;
-      this.progress = data.progress.percent;
-      this.speed = data.speedAverageHumanized;
-    });
-  }
+  constructor() { }
 
   ngOnInit() {
-    this.url = this.config.apiUrl+"/files";
-    this.basicOptions  = {url: this.url, calculateSpeed: true };
-    this.zone = new NgZone({ enableLongStackTrace: false });
-  }
-
-  public navigateToShare(){
-    // Validate location code
-    if ( this.locationCode ){
-      if ( this.locationCode.match(/^[a-f0-9]{10}$/i) == null )
-      {
-          alert("Code: "+this.locationCode+" is invalid, it must only contain the characters A-F and 0-9")
-          return;
-      }
-
-      // Verify the location code exists
-      
-    }
   }
 }
