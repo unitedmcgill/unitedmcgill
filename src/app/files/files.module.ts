448cdd78dea9083f29e6a1b8ed2ac51af962a58d
jecourtney <JCourtney@unitedmcgill.com>
2017-03-23 10:04:44 -0400

    fixed files

--- src/app/files/files.module.ts
+++ src/app/files/files.module.ts
@@ -5,8 +5,6 @@ import { TabsModule }               from 'ng2-bootstrap/components/tabs';
 
 import { FilesComponent }       from './files.component';
 import { FilesRoutingModule }   from './files-routing.module';
-// import { FileSelectDirective, FileDropDirective } from 'ng2-file-upload/ng2-file-upload';
-import { UPLOAD_DIRECTIVES } from 'ng2-uploader/ng2-uploader';
 
 @NgModule({
     imports: [
@@ -16,10 +14,7 @@ import { UPLOAD_DIRECTIVES } from 'ng2-uploader/ng2-uploader';
         TabsModule
     ],
     declarations: [ 
-        FilesComponent,
-        UPLOAD_DIRECTIVES
-        //FileSelectDirective,
-        //FileDropDirective
+        FilesComponent
     ]
 })
 export class FilesModule { }
