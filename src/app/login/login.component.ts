268d5e962b5f0eb193bdd0ebed1d5488d75bf64e
jecourtney <JCourtney@unitedmcgill.com>
2017-08-08 15:29:53 -0400

    Before obliterate

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,40 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApplicationUser } from '../models/applicationuser';
+ 
+import { AuthenticationService } from '../services/auth.service';
+ 
+@Component({
+    moduleId: module.id,
+    templateUrl: 'login.component.html'
+})
+ 
+export class LoginComponent implements OnInit {
+    model: ApplicationUser = {userName: 'Administrator', password:''};
+    loading = false;
+    error = '';
+ 
+    constructor(
+        private router: Router,
+        private authenticationService: AuthenticationService) { }
+ 
+    ngOnInit() {
+        // reset login status
+        this.authenticationService.logout();
+    }
+ 
+    login() {
+        this.loading = true;
+        this.authenticationService.login(this.model)
+            .subscribe(result => {
+                if (result === true) {
+                    // login successful
+                    this.router.navigate([this.authenticationService.url]);
+                } else {
+                    // login failed
+                    this.error = 'Username or password is incorrect';
+                    this.loading = false;
+                }
+            });
+    }
+}
